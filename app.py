2b625a30e3da3e0eaaa01455f173d0db91615c9f
vans123345454 <119332658+vans123345454@users.noreply.github.com>
2022-11-28 20:05:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ st.write("""
 st.header('User Input Parameters')
 
 def user_input_features():
-    num1 = st.number_input("Numerator",step=1)
-    num2 = st.number_input("Denominator",step=1)
-    return f'Division of {num1} and {num2} is {num1/num2}' 
+    try:
+        num1 = st.number_input("Numerator",step=1)
+        num2 = st.number_input("Denominator",step=1)
+        return f'Division of {num1} and {num2} is {num1/num2}' 
+    except ZeroDivisionError:
+        return f'Denominator cannot be 0'
     
 
 result = user_input_features()
